8061a792f60c8a9fc6bf4df37e82907f17577e74
carlzenriques <carluxhenrique@gmail.com>
2020-01-29 09:59:36 -0300

    Property, Event, Style and Class Binding

---
+++ Data-Binding/src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+
+  url: String = 'http://google.com.br';
+  urlImagem = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/512px-Angular_full_color_logo.svg.png';
+  valorAtual: String = '';
+  valorSalvo: String = '';
+  isMouseOver: Boolean = false;
+
+  botaoClicado() {
+    alert('Botao Clicado!');
+  }
+
+  onKeyUp(evento: KeyboardEvent) {
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+
+  salvarValor(valor) {
+    this.valorSalvo = valor;
+
+  }
+
+  onMouseOverOut() {
+    this.isMouseOver = !this.isMouseOver;
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
